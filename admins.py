86d81faf332fe7cf8d78fafd1144de90c0b2fd7e
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 00:52:50 +0500

    Update admins.py

--- admins.py
+++ admins.py
@@ -1,1 +1,13 @@
+from telegram import ChatMemberAdministrator, ChatMemberOwner
 
+
+async def is_admin(chat, user_id: int) -> bool:
+    member = await chat.get_member(user_id)
+
+    return isinstance(
+        member,
+        (
+            ChatMemberAdministrator,
+            ChatMemberOwner
+        )
+    )
