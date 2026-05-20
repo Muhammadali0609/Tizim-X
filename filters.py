b9c90632dcf1b8dd266fb3826a220573f427ff17
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 00:36:51 +0500

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,1 +1,13 @@
+import re
 
+URL_PATTERN = re.compile(
+    r"(https?://|www\.|t\.me/|telegram\.me/|@\w+)",
+    re.IGNORECASE
+)
+
+
+def has_link(text: str) -> bool:
+    if not text:
+        return False
+
+    return bool(URL_PATTERN.search(text))
