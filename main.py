6a9ed35fd032d8ea442780845eda4c3206faf637
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 04:46:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ChatMemberHandler, MessageHandler, filters
-
+from telegram import BotCommand
 from config import BOT_TOKEN, WEBHOOK_URL, PORT
 from handlers import start_command, language_callback, bot_added_to_group, check_group_message, set_group_language
 from db import setup_database
 
+async def setup_commands(app):
+    commands = [
+        BotCommand("ru", "Русский язык группы"),
+        BotCommand("uz", "Guruh tili o‘zbekcha"),
+    ]
+
+    await app.bot.set_my_commands(commands)
 
 def main():
     setup_database()
     app = Application.builder().token(BOT_TOKEN).build()
+    app.post_init = setup_commands
 
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CallbackQueryHandler(language_callback, pattern="^lang_"))
