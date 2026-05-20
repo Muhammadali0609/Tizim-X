08b754f1b7c0a78c68bbb14377f4e518b4379b31
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-20 19:05:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from telegram.ext import (
+    Application,
+    CommandHandler,
+)
 
+from config import BOT_TOKEN, WEBHOOK_URL, PORT
+from handlers import start_command
+
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start_command))
+
+    print("TizimX started...")
+
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        webhook_url=f"{WEBHOOK_URL}/webhook",
+        secret_token=BOT_TOKEN,
+    )
+
+
+if __name__ == "__main__":
+    main()
