22793b138ff58c8068829c8fd1ae338eef1eee60
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 00:01:11 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,8 +1,29 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
 from texts import TEXTS
 
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(TEXTS["ru"]["start"])
+    keyboard = [
+        [
+            InlineKeyboardButton("🇷🇺 Русский", callback_data="lang_ru"),
+            InlineKeyboardButton("🇺🇿 O‘zbekcha", callback_data="lang_uz"),
+        ]
+    ]
+
+    await update.message.reply_text(
+        TEXTS["ru"]["choose_language"],
+        reply_markup=InlineKeyboardMarkup(keyboard),
+    )
+
+
+async def language_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    lang = query.data.replace("lang_", "")
+
+    await query.edit_message_text(
+        TEXTS[lang]["language_saved"] + "\n\n" + TEXTS[lang]["start"]
+    )
