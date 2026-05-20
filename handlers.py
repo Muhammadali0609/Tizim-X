d0dfb10e0a651ea01d71cf4d043838af8905822b
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-20 19:21:27 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,1 +1,8 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+from texts import TEXTS
+
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(TEXTS["ru"]["start"])
