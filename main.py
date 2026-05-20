ecd644208cf6a2495e4595fecfa662651229244b
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 00:10:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 
 from config import BOT_TOKEN, WEBHOOK_URL, PORT
 from handlers import start_command, language_callback
+from db import setup_database
 
 
 def main():
+    setup_database()
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start_command))
@@ -16,7 +18,6 @@ def main():
         url_path="webhook",
         webhook_url=f"{WEBHOOK_URL}/webhook",
     )
-
 
 if __name__ == "__main__":
     main()
