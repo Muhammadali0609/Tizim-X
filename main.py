967901c59311dd2223f7bca93aab23d7f98fedac
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 11:12:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
     app.add_handler(MessageHandler(filters.StatusUpdate.DELETE_CHAT_PHOTO, clean_service_message))
     app.add_handler(MessageHandler(filters.StatusUpdate.PINNED_MESSAGE, clean_service_message))
     app.add_handler(CallbackQueryHandler(check_subscription_callback, pattern="^check_sub:"))
-    app.add_handler(MessageHandler(filters.Regex("^(⚙️ Настройки|⚙️ Sozlamalar)$"), settings_button_handler))
+    app.add_handler(MessageHandler(filters.Regex("^(⚙️ Управлять|⚙️ Boshqarish)$"), settings_button_handler))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_group_message))
     app.add_handler(CommandHandler("ru", set_group_language))
     app.add_handler(CommandHandler("uz", set_group_language))
