10509c372225bd7ede3c11c511953ae967f83d3b
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 16:15:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ from handlers import (start_command,
     ad_links_panel_callback,
     add_ad_link_callback,
     bad_words_search_callback,
-    delete_bad_word_callback
+    delete_bad_word_callback,
+    ad_phrases_panel_callback,
+    add_ad_phrase_callback,
 )
 from db import setup_database
 
@@ -71,6 +73,9 @@ def main():
     app.add_handler(CallbackQueryHandler(add_ad_link_callback, pattern="^ad_links_add:"))
     app.add_handler(CallbackQueryHandler(bad_words_search_callback, pattern="^bad_words_search:"))
     app.add_handler(CallbackQueryHandler(delete_bad_word_callback, pattern="^delete_bad_word:"))
+    app.add_handler(CallbackQueryHandler(ad_phrases_panel_callback, pattern="^ad_phrases_panel:"))
+    app.add_handler(CallbackQueryHandler(ad_phrases_panel_callback, pattern="^ad_phrases_page:"))   
+    app.add_handler(CallbackQueryHandler(add_ad_phrase_callback, pattern="^ad_phrases_add:"))
 
     app.run_webhook(
         listen="0.0.0.0",
