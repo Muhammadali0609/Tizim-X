de85a17c46f66a18bfd2e72a17c11fd4a33c75ed
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 07:25:33 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,14 +1,19 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ChatPermissions
 from telegram.constants import ChatMemberStatus
 from telegram.ext import ContextTypes
-from db import save_user_language, save_group, get_group_settings, get_group_language, save_group_language, get_required_channel, save_group_admin
+from db import save_user_language, save_group, get_group_settings, get_group_language, save_group_language, get_required_channel, save_group_admin, get_user_groups
 from texts import TEXTS
 from filters import has_link, has_bad_word
 from admins import is_admin
 import asyncio
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
+    keyboard = ReplyKeyboardMarkup(
+        [["⚙️ Настройки"]],
+        resize_keyboard=True
+    )
+
+    lang_keyboard = [
         [
             InlineKeyboardButton("🇷🇺 Русский", callback_data="lang_ru"),
             InlineKeyboardButton("🇺🇿 O‘zbekcha", callback_data="lang_uz"),
@@ -17,22 +22,27 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(
         TEXTS["ru"]["choose_language"],
-        reply_markup=InlineKeyboardMarkup(keyboard),
+        reply_markup=keyboard
     )
 
-
+    await update.message.reply_text(
+        TEXTS["ru"]["choose_language"],
+        reply_markup=InlineKeyboardMarkup(lang_keyboard)
+    )
+    
 async def language_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    await query.answer()
-    
-    user_id = query.from_user.id
+
     lang = query.data.replace("lang_", "")
+    user_id = query.from_user.id
+
     save_user_language(user_id, lang)
 
-    await query.edit_message_text(
-        TEXTS[lang]["language_saved"] + "\n\n" + TEXTS[lang]["start"]
-    )
+    await query.answer(TEXTS[lang]["language_saved"])
 
+    await query.edit_message_text(
+        TEXTS[lang]["start"]
+    )
 async def bot_added_to_group(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.my_chat_member
 
