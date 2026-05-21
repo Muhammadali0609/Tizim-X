c0d20da6d0f24eb077d7de986e9884e5f894d557
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-05-21 17:05:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ from handlers import (start_command,
     add_ad_phrase_callback,
     delete_ad_link_start_callback,
     delete_ad_phrase_start_callback,
+    ad_exceptions_panel_callback,
+    add_ad_exception_callback,
+    delete_ad_exception_start_callback,
 )
 from db import setup_database
 
@@ -80,6 +83,10 @@ def main():
     app.add_handler(CallbackQueryHandler(add_ad_phrase_callback, pattern="^ad_phrases_add:"))
     app.add_handler(CallbackQueryHandler(delete_ad_link_start_callback, pattern="^ad_links_delete:"))
     app.add_handler(CallbackQueryHandler(delete_ad_phrase_start_callback, pattern="^ad_phrases_delete:"))
+    app.add_handler(CallbackQueryHandler(ad_exceptions_panel_callback, pattern="^ad_exceptions_panel:"))
+    app.add_handler(CallbackQueryHandler(ad_exceptions_panel_callback, pattern="^ad_exceptions_page:"))
+    app.add_handler(CallbackQueryHandler(add_ad_exception_callback, pattern="^ad_exceptions_add:"))
+    app.add_handler(CallbackQueryHandler(delete_ad_exception_start_callback, pattern="^ad_exceptions_delete:"))
 
     app.run_webhook(
         listen="0.0.0.0",
