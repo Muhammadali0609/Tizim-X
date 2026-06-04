229f6e8c1574775c0624ae87d91fdf70b2fe7d5e
Muhammadali0609 <odiljonovmuhammadali111@gmail.com>
2026-06-04 13:59:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,6 @@ from handlers import (start_command,
     payment_tariff_standard_callback,
     payment_toggle_group_callback,
     payment_create_callback,
-    payment_test_paid_callback,
     payment_cancel_callback,
 )
 from db import setup_database
@@ -264,7 +263,6 @@ def main():
     app.add_handler(CallbackQueryHandler(payment_tariff_standard_callback, pattern="^payment_tariff_standard$"))
     app.add_handler(CallbackQueryHandler(payment_toggle_group_callback, pattern="^payment_toggle_group:"))
     app.add_handler(CallbackQueryHandler(payment_create_callback, pattern="^payment_create$"))
-    app.add_handler(CallbackQueryHandler(payment_test_paid_callback, pattern="^payment_test_paid:"))
     app.add_handler(CallbackQueryHandler(payment_cancel_callback, pattern="^payment_cancel:"))
     
     app.run_webhook(
